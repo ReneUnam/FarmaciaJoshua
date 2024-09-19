fc82b0b514a1b983e4698751943c39f2ddc80945
Sizm12 <s.zeledon.cbe@gmail.com>
2024-09-18 21:43:15 -0600

    Creando un Controlado para CRUD con Archivo JSON

---
+++ Services/WebApi.Interface/IMaestroService.cs
@@ -0,0 +1,13 @@
+﻿using WebApi.Model;
+
+namespace WebApi.Interface;
+
+public interface IMaestroService
+{
+
+    public MaestroEntities Add(MaestroEntities maestro);
+    public List<MaestroEntities> GetALL();
+    public MaestroEntities GetByID(int id);
+    void Update(MaestroEntities maestro);
+    void Delete(int id);
+}
--- WebApi.Api/Program.cs
+++ WebApi.Api/Program.cs
@@ -4,11 +4,15 @@ using WebApi.Implementation;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-builder.Services.AddScoped<IEstudianteService, EstudianteService>();
+builder.Services.AddSingleton<IEstudianteService, EstudianteService>();
+builder.Services.AddScoped<IMaestroService, MaestroService>();
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 app.UseRouting();
 app.MapControllers();
+app.UseSwagger();
+app.UseSwaggerUI();
 app.Run();
