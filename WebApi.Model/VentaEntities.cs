bd656dc05c4a4cbe4a7524deec526b3c03ca453f
ReneUnan <rene.sandoval23908226@estu.unan.edu.ni>
2024-11-08 20:28:49 -0600

    agregando entidades para el maestro detalle

---
+++ WebApi.Model/VentaEntities.cs
@@ -0,0 +1,20 @@
+namespace WebApi.Model;
+
+public class Venta
+{
+    public int IdVenta { get; set; }
+    public int IdCliente { get; set; }
+    public int IdUsuario { get; set; }
+    public DateTime FechaVenta {get; set;}
+    public decimal Total { get; set; }
+    public List<DetalleVenta> VentaDetalle { get; set; } = new List<DetalleVenta>();
+}
+
+public class DetalleVenta{
+    public int IdDetalleVenta { get; set; }
+    public int Venta { get; set; }
+    public int IdProducto { get; set; } 
+    public int Cantidad { get; set; }
+    public int PrecioUnitario { get; set; }
+    public int Subtotal { get; set; } 
+}
