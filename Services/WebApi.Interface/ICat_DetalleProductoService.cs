1a6485e7e2f0f9e641df9b93af49049063563e3e
luisitoloco <luis.lopez23908589@estu.unan.edu.ni>
2024-11-25 20:17:28 -0600

    Detalles

---
+++ Services/WebApi.Interface/ICat_DetalleProductoService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Model;
+
+namespace WebApi.Interface
+{
+    public interface ICat_DetalleProductoEntities
+    {
+        public Cat_DetalleProductoEntities Add(Cat_DetalleProductoEntities cat_detalleproducto);
+        public IEnumerable<Cat_DetalleProductoEntities> GetAll();
+        public Cat_DetalleProductoEntities GetById(int id);
+        public void Update(Cat_DetalleProductoEntities cat_detalleproducto);
+        public void Delete(int id);
+    }
+}
